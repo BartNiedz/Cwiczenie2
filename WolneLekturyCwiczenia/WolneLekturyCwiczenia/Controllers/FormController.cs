90780250c656a0b490c73b2a8d1c10f095737fe1
unknown <bartosz.niedzwiecki96.com>
2024-04-02 20:36:14 +0200

    punkt 3.

---
+++ WolneLekturyCwiczenia/WolneLekturyCwiczenia/Controllers/FormController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WolneLekturyCwiczenia.Models;
+
+namespace WolneLekturyCwiczenia.Controllers
+{
+    public class FormController : Controller
+    {
+        public IActionResult FormView()
+        {
+            return View();
+        }
+       
+        [HttpPost]
+        public IActionResult SaveFormData(FormData data)
+        {
+            string email = data.email;
+            string firstName = data.firstName;
+            string lastName = data.lastName;
+            string textarea = data.textarea;
+            
+
+            return View();
+        }
+    }
+}
---
+++ WolneLekturyCwiczenia/WolneLekturyCwiczenia/Models/FormData.cs
@@ -0,0 +1,12 @@
+﻿namespace WolneLekturyCwiczenia.Models
+{
+    public class FormData
+    {
+      
+        public string email { get; set; }
+        public string firstName  { get; set; }
+        public string lastName { get; set; }
+
+        public string textarea { get; set; }
+    }
+}
