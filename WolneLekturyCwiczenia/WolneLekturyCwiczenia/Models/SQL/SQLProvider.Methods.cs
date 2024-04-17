7ac12d1acd16dd590912ae5e8f8a770d96c99f39
Krzysiek <46751932+kzyq@users.noreply.github.com>
2024-04-17 12:01:02 +0200

    - mysql

---
+++ WolneLekturyCwiczenia/WolneLekturyCwiczenia/Models/SQL/SQLProvider.Methods.cs
@@ -0,0 +1,17 @@
+﻿using NPoco;
+using WolneLekturyCwiczenia.Models.SQL.Table;
+
+namespace WolneLekturyCwiczenia.Models.SQL
+{
+    partial class SQLProvider
+    {
+
+        public void SaveTestowa(Testowa model)
+        {
+            using (IDatabase conection = GetDatabase())
+            {
+                conection.Save(model);
+            }
+        }
+    }
+}
---
+++ WolneLekturyCwiczenia/WolneLekturyCwiczenia/Models/SQL/Table/Testowa.cs
@@ -0,0 +1,12 @@
+﻿using NPoco;
+
+namespace WolneLekturyCwiczenia.Models.SQL.Table
+{
+    [TableName("testowa")]
+    [PrimaryKey("Id", AutoIncrement = true)]
+    public class Testowa
+    {
+        public int Id { get; set; }
+        public string Message { get; set; }
+    }
+}
